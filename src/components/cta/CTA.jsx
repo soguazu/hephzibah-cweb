d1348c6f682040e97a5f33cbacce71360d2c0142
soguazu <deareststanley@gmail.com>
2022-05-14 11:51:47 +0100

    cta

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+// import { styled } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import './cta.css'
 
